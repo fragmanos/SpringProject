ed371e2155b348a34c44681dd15f43bf7c118020
fragmanos <fragmanos@gmail.com>
2015-07-25 23:20:25 +0100

    http://spring.io/guides/gs/consuming-rest/ fetch quotes about Spring Boot returned as a JSON document.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,28 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 
     public static void main(String[] args) throws Exception{
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
     }
 }
